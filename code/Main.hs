87bcac5cd8eed28dd4e2f9906635d7116ba6b589
SEbbaDK <sebbadk@gmail.com>
2022-05-19 12:05:19 +0200

    code: Update CLI to reflect new prettyprinting

--- code/Main.hs
+++ code/Main.hs
@@ -13,44 +13,48 @@ import           Text.Megaparsec.Error (errorBundlePretty)
 import           TypeChecker           (typeCheck)
 import           Types
 import           Binding               (bindCheck)
+import Pretty
 
 putErr :: String -> IO ()
 putErr = hPutStr stderr
 
 data Args
   = Args
-    { file       :: String
-    , printast   :: Bool
-    , printlatex :: Bool
-    , printBinds :: Bool
+    { file        :: String
+    , printast    :: Bool
+    , printpretty :: Bool
+    , printlatex  :: Bool
+    , printbinds  :: Bool
     }
 
 parser :: Parser Args
 parser =
   Args
   <$> argument str (metavar "FILE")
-  <*> switch (long "print-ast"   <> short 'a' <> help "Print the AST")
-  <*> switch (long "print-latex" <> short 'l' <> help "Print latex output")
-  <*> switch (long "print-types" <> short 't' <> help "Print binding types")
+  <*> switch (long "print-ast"    <> short 'a' <> help "Print the AST")
+  <*> switch (long "print-pretty" <> short 'p' <> help "Pretty-print the specification")
+  <*> switch (long "print-latex"  <> short 'l' <> help "Print latex output")
+  <*> switch (long "print-types"  <> short 't' <> help "Print binding types")
 
 main :: IO ()
 main = cli =<< execParser (info (parser <**> helper) (fullDesc <> progDesc "test" <> header "test2"))
 
 cli :: Args -> IO ()
-cli Args {file, printast, printlatex = False, printBinds} = do
+cli Args {file, printast, printlatex = False, printpretty, printbinds} = do
   src <- readFile file
   case doParse file src of
     Left err ->
       putErr $ "Parsing Error: " ++ errorBundlePretty err
     Right ast -> do
-      when printast (print ast)
+      when printast (putStrLn $ show ast)
+      when printpretty (putStrLn $ pprint ast)
       putStrLn $ case bindCheck ast of
         Nothing -> "No bind errors"
-        Just e -> unlines e
+        Just e -> "Bind errors:\n" ++ unlines e
       case typeCheck ast of
         Right allBinds  -> do
           putStrLn "Checks passed"
-          when printBinds (forM_ allBinds (putStrLn . showBinds))
+          when printbinds (forM_ allBinds (putStrLn . showBinds))
         Left err -> do
           putErr $ "Type Error: " ++ showErrorMessage src err
           -- putErrLn $ showErrorInSource err src
@@ -60,3 +64,4 @@ cli Args {file, printast, printlatex = True} = do
 
 showBinds :: Map String Type -> String
 showBinds = foldlWithKey (\a name type_ -> a ++ "\n" ++ name ++ " :: " ++ show type_) []
+
